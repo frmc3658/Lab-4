a8407c22d0e7c161e42fdd9da7e8b00981a49f7f
frmc3658 <frmc3658@colorado.edu>
2022-09-29 12:50:18 -0600

    Added comments

--- pwm.c
+++ pwm.c
@@ -1,49 +1,68 @@
 /*
  * pwm.c
  *
- *  Created on: Sep 19, 2022
- *      Author: Arielle
  */
+
+//***************************************************************
+// included files
+//**************************************************************/
 #include "pwm.h"
 
+//***************************************************************
+// function definitions
+//**************************************************************/
 void start_pwm(void)
 {
-    TIMER_A0 -> CTL |= TIMER_A_CTL_MC__UP;// set MC to up
+    // set MC to up mode
+    TIMER_A0->CTL |= TIMER_A_CTL_MC__UP;
 }
+
 
 void stop_pwm(void)
 {
-    TIMER_A0 -> CTL = TIMER_A_CTL_MC__STOP;// set MC to halt, stop
+    // set MC to halt/stop mode
+    TIMER_A0->CTL = TIMER_A_CTL_MC__STOP;
 }
+
 
 void config_pwm_timer(void)
 {
-    TIMER_A0->CTL |= TIMER_A_CTL_CLR;           // clear R register
-    TIMER_A0->CTL |= TIMER_A_CTL_SSEL__SMCLK;   // select SMCLK as source
-    TIMER_A0->CTL |= TIMER_A_CTL_ID_1;          // set input divider
+    // clear R register
+    TIMER_A0->CTL |= TIMER_A_CTL_CLR;
 
-    //want known state + NOR glitch occurs so use TIMER_A_CCTLN_OUTMOD_7 as intermediary state
-    TIMER_A0 -> CCTL[1] |= TIMER_A_CCTLN_OUTMOD_7; // NOR glitch set 1111 MODE
-    TIMER_A0 -> CCTL[1] &= TIMER_A_CCTLN_OUTMOD_3; // Set/Reset mode
+    // select SMCLK as source
+    TIMER_A0->CTL |= TIMER_A_CTL_SSEL__SMCLK;
 
+    // set input divider; 2^1
+    TIMER_A0->CTL |= TIMER_A_CTL_ID_1;
+
+    // intermediary state; see note at bottom of page (TRM 19.2.5.1.3)
+    TIMER_A0 -> CCTL[1] |= TIMER_A_CCTLN_OUTMOD_7;
+
+    // Set/Reset mode
+    TIMER_A0 -> CCTL[1] &= TIMER_A_CCTLN_OUTMOD_3;
+
+    // set capture/compare register 0 value
     TIMER_A0 -> CCR[0] = TICKS;
 }
 
 
 void config_pwm_timer_25(void)
 {
+    // set capture/compare reg1 value value for 25% duty cycle
     TIMER_A0->CCR[1] = CCR1_DUTY_25;
 }
 
 
 void config_pwm_timer_50(void)
 {
+    // set capture/compare reg1 value value for 50% duty cycle
     TIMER_A0->CCR[1] = CCR1_DUTY_50;
 }
 
 
 void config_pwm_timer_75(void)
 {
+    // set capture/compare reg1 value for 75% duty cycle
     TIMER_A0->CCR[1] = CCR1_DUTY_75;
 }
-
--- pwm.h
+++ pwm.h
@@ -1,27 +1,37 @@
 /*
  * pwm.h
  *
- *  Created on: Sep 19, 2022
- *      Author: Arielle
  */
 
+//***************************************************************
+// header guards
+//**************************************************************/
 #ifndef PWM_H_
 #define PWM_H_
+
+//***************************************************************
+// included files
+//**************************************************************/
 #include "msp.h"
 
+//***************************************************************
+// defined macros
+//**************************************************************/
 #define TICKS               ((uint16_t) 0xBB8)
 #define CCR1_DUTY_25        ((uint16_t) 0x177)
 #define CCR1_DUTY_50        ((uint16_t) 0x2EE)
 #define CCR1_DUTY_75        ((uint16_t) 0x465)
 
+//***************************************************************
+// function prototypes
+//**************************************************************/
 
-void config_pwm_timer(void);
+
 void stop_pwm(void);
 void start_pwm(void);
-
+void config_pwm_timer(void);
 void config_pwm_timer_25(void);
 void config_pwm_timer_50(void);
 void config_pwm_timer_75(void);
-
 
 #endif /* PWM_H_ */
