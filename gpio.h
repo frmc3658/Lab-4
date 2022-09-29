fc92d30131af171ecc0da36f4be67b5608832917
frmc3658 <frmc3658@colorado.edu>
2022-09-29 13:09:26 -0600

    Added GPIO

--- gpio.h
+++ gpio.h
@@ -1,13 +1,13 @@
 /*
- * pwm.h
+ * gpio.h
  *
  */
 
 //***************************************************************
 // header guards
 //**************************************************************/
-#ifndef PWM_H_
-#define PWM_H_
+#ifndef GPIO_H_
+#define GPIO_H_
 
 //***************************************************************
 // included files
@@ -17,19 +17,12 @@
 //***************************************************************
 // defined macros
 //**************************************************************/
-#define TICKS               ((uint16_t) 0xBB8)
-#define CCR1_DUTY_25        ((uint16_t) 0x177)
-#define CCR1_DUTY_50        ((uint16_t) 0x2EE)
-#define CCR1_DUTY_75        ((uint16_t) 0x465)
+
 
 //***************************************************************
 // function prototypes
 //**************************************************************/
-void stop_pwm(void);
-void start_pwm(void);
-void config_pwm_timer(void);
-void config_pwm_timer_25(void);
-void config_pwm_timer_50(void);
-void config_pwm_timer_75(void);
+void config_gpio(void);
 
-#endif /* PWM_H_ */
+
+#endif /* GPIO_H_ */
--- main.c
+++ main.c
@@ -1,29 +1,42 @@
-#include "msp.h"
-#include "pwm.h"
-
 /**
  * main.c
  */
+
+//***************************************************************
+// included files
+//**************************************************************/
+#include "msp.h"
+#include "pwm.h"
+#include "gpio.c"
+
+//***************************************************************
+// main loop
+//**************************************************************/
 void main(void)
 {
-	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
-	stop_pwm(); //KNOWN STATE
-	config_pwm_timer(); //only run once
+    // stop watchdog timer
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;
 
-	//todo config gpio
-	P2->DIR |= BIT4;
-	P2->SEL0 |= BIT4;
-	P2->SEL1 &= ~BIT4;
-    P2->OUT &= ~(BIT4);
+	// stop timer_a before modifying operation; per: TRM 19.2.1
+	stop_pwm();
 
+	// configure timer_a for PWM operation
+	config_pwm_timer();
+
+	// configure P2.4 for PWM
+	config_gpio();
+
+	// start PWM timer
 	start_pwm();
+
+
 	//config_pwm_timer_25();
     //config_pwm_timer_50();
 	config_pwm_timer_75();
 
 	while(1)
 	{
-
+	    // main infinite while loop
 	}
 
 }
